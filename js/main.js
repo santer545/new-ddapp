19f541420f3cf38859b2541fbca7151c4d456a5f
Serhii Panchenko <spanchenko@mycredit.com.ua>
2017-04-26 17:46:36 +0200

    26.04.2017

--- js/main.js
+++ js/main.js
@@ -931,7 +931,7 @@ function handleFileSelect(evt) {
                 // Render thumbnail.
                 var span = document.createElement('li');
                 span.innerHTML = [
-                    '<div class="img-delete"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="#FFF" fill-rule="nonzero" d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"/></svg></div><img style="width: 113px;" src="',
+                    '<div class="img-delete"><img src="assets/png/cancel.png"></div><img style="width: 113px;" src="',
                     e.target.result,
                     '" title="', escape(theFile.name),
                     '"/>'
